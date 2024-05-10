80f72df45bad72fe493c08f8427217b49fa23464
Abhishek Rawat <126187746+abhirawat24@users.noreply.github.com>
2024-05-11 01:17:36 +0530

    Create dbwithExpress.js

---
+++ dbwithExpress.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://abhirawat102:YbPxHC5AaBf9aKJW@cluster0.jpnq584.mongodb.net/useraddition?retryWrites=true&w=majority");
+
+const User = mongoose.model('Users', { name: String, email: String, password: String });
+
+app.post("/signup", async function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+  const name = req.body.name;
+
+  const existingUser = await User.findOne({email: username});
+  if (existingUser){
+    return res.status(400).send('Username already exists');
+  }
+  const user = new user({
+    name: name,
+    email: email,
+    password: password
+  });
+  user.save();
+  res.json({
+    msg: "User created successfully"
+  })
+})
