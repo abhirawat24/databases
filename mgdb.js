b9f4b79986457368c383de444284cb095998e08e
Abhishek Rawat <126187746+abhirawat24@users.noreply.github.com>
2024-05-11 00:49:51 +0530

    Create mgdb.js

---
+++ mgdb.js
@@ -0,0 +1,9 @@
+//Learning mongoDB
+//adding new user to the database
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://abhirawat102:YbPxHC5AaBf9aKJW@cluster0.jpnq584.mongodb.net/userappnew");
+
+const User = mongoose.model('Users', { name: String, email: String, password: String });
+
+const newUser = new User({ name: 'abhi', email: 'xcvkp@example.com', password: 'abhi@123' });
+newUser.save().then(() => console.log('saved')).catch(err => console.error(err));
